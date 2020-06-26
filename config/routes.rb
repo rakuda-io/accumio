1aa341a1659b88bbdfa94bd139a7a214b015d814
Yuk <rakudalms@gmail.com>
2020-06-26 12:30:11 +0900

    basic System

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  root to: 'home#top'
   devise_for :users
+  resource :user, except: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
