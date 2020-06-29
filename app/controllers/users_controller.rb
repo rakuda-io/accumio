ce6681d47279f49a1d08851a47913a11dae79fc3
Yuk <rakudalms@gmail.com>
2020-06-29 13:31:06 +0900

    create users indexr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all.order(id: 'DESC')
+  end
+
   def show
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#top'
   devise_for :users
-  resource :user, except: [:new, :create, :destroy]
+  resource :user, only: [:new, :create, :edit, :update, :show, :destroy]
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
